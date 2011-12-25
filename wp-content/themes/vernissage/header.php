1b43f713e6c017055b285220d81a829860df5f1c
Oleksiy Solodovnyk <oleksiy.solodovnyk@gmail.com>
2011-12-25 20:27:25 +0200

    change slider, add news to index page

--- wp-content/themes/vernissage/header.php
+++ wp-content/themes/vernissage/header.php
@@ -24,11 +24,12 @@
 	?></title>
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
-    
+        
     <script type="text/javascript" src="http://userapi.com/js/api/openapi.js?45"></script>
     <script type="text/javascript">
       VK.init({apiId: 2718494, onlyWidgets: true});
     </script>
+    <?php wp_head(); ?> 
 </head>
 <body>
     <div id="wrapper">
